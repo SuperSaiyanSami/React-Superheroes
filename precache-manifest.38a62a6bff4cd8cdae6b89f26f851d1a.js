12c7027ef7126c44fa4070c51db6657dfb5300d6
syedsami8 <ssamiuddin007@gmail.com>
2018-11-11 15:19:48 +0530

    Updates

---
+++ precache-manifest.38a62a6bff4cd8cdae6b89f26f851d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "369d019823314bba0c2d",
+    "url": "/React-Superheroes/static/js/runtime~main.369d0198.js"
+  },
+  {
+    "revision": "3896dafb8a3b1bac53b8",
+    "url": "/React-Superheroes/static/js/main.3896dafb.chunk.js"
+  },
+  {
+    "revision": "1670935a042b1fdc38db",
+    "url": "/React-Superheroes/static/js/1.1670935a.chunk.js"
+  },
+  {
+    "revision": "3896dafb8a3b1bac53b8",
+    "url": "/React-Superheroes/static/css/main.7c6a8f7f.chunk.css"
+  },
+  {
+    "revision": "9d42b366f7a5291dec554ce4441ce67e",
+    "url": "/React-Superheroes/index.html"
+  }
+];
